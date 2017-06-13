8068890270d7d6cfb7229f537ed1df1d8f77ebd5
Nolonwabo <nolonwabomahanjana13@gmail.com>
2017-06-13 18:55:23 +0200

    another changes

--- main.js
+++ main.js
@@ -114,83 +114,83 @@ container.addEventListener('click', function(evt){
 //   Catergory:
 // }];
 
-var template = document.querySelector('.myTemplate').innerHTML;
-var myTemp = Handlebars.compile(template);
+// var template = document.querySelector('.myTemplate').innerHTML;
+// var myTemp = Handlebars.compile(template);
+//
+// var itemList = [{
+//       totalBudget: "1000",
+//       IconPicture: "fish",
+//       Price: "150",
+//       Catergory:"Need"
+//     },
+//     totalBudget: "1000",
+//     IconPicture: "cereals",
+//     Price: "50",
+//     Catergory: "Need",
+//   },
+//   totalBudget: "1000",
+// IconPicture: "cheese",
+//   Price: "45",
+//   Catergory: "Need"
+// },
+// totalBudget: "1000",
+// IconPicture: "earings",
+//   Price: "120",
+//   Catergory: "Want"
+// },
+// totalBudget: "1000",
+// IconPicture: "handbag",
+//   Price: "300",
+//   Catergory: "Want"
+// },
+// totalBudget: "1000",
+// IconPicture: "clothes",
+//   Price: "1000",
+//   Catergory: "Want"
+// },
+// totalBudget: "1000",
+// IconPicture: "shirt",
+//   Price: "300",
+//   Catergory: "Want"
+// },
+// totalBudget: "1000",
+// IconPicture: "hawaiin",
+//   Price: "250",
+//   Catergory: "Want"
+// },
+// totalBudget: "1000",
+// IconPicture: "smartphone",
+//   Price: "3500",
+//   Catergory: "Luxury"
+// },
+// totalBudget: "1000",
+// IconPicture: "broccoli",
+//   Price: "120",
+//   Catergory: "Need"
+// },
+// totalBudget: "1000",
+// IconPicture: "salad",
+//   Price: "150",
+//   Catergory: "Want"
+// },
+// totalBudget: "1000",
+// IconPicture: "apple",
+//   Price: "45",
+//   Catergory:"Need"
+// }];
 
-var itemList = [{
-      totalBudget: "1000",
-      IconPicture: "fish",
-      Price: "150",
-      Catergory:"Need"
-    },
-    totalBudget: "1000",
-    IconPicture: "cereals",
-    Price: "50",
-    Catergory: "Need",
-  },
-  totalBudget: "1000",
-IconPicture: "cheese",
-  Price: "45",
-  Catergory: "Need"
-},
-totalBudget: "1000",
-IconPicture: "earings",
-  Price: "120",
-  Catergory: "Want"
-},
-totalBudget: "1000",
-IconPicture: "handbag",
-  Price: "300",
-  Catergory: "Want"
-},
-totalBudget: "1000",
-IconPicture: "clothes",
-  Price: "1000",
-  Catergory: "Want"
-},
-totalBudget: "1000",
-IconPicture: "shirt",
-  Price: "300",
-  Catergory: "Want"
-},
-totalBudget: "1000",
-IconPicture: "hawaiin",
-  Price: "250",
-  Catergory: "Want"
-},
-totalBudget: "1000",
-IconPicture: "smartphone",
-  Price: "3500",
-  Catergory: "Luxury"
-},
-totalBudget: "1000",
-IconPicture: "broccoli",
-  Price: "120",
-  Catergory: "Need"
-},
-totalBudget: "1000",
-IconPicture: "salad",
-  Price: "150",
-  Catergory: "Want"
-},
-totalBudget: "1000",
-IconPicture: "apple",
-  Price: "45",
-  Catergory:"Need"
-}];
 
+//function submitButton() {
+  //for (var i = 0; i < itemList.length; i++) {
+  //  var catagoryList = itemList[i];
+  //}
 
-function submitButton() {
-  for (var i = 0; i < itemList.length; i++) {
-    var catagoryList = itemList[i];
-  }
+//if(Catergory === Luxury && Price <= 100){
 
-if(Catergory === Luxury && Price <= 100){
+//}
 
-}
+//  if (Catergory === Luxury && itemList.Price <= 100) {
 
-  if (Catergory === Luxury && itemList.Price <= 100) {
+  //}
 
-  }
-
-}
+//}
