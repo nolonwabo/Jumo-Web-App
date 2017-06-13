18e4a2eac35ed010ccf178f21ac8601d54bf5ef8
Nolonwabo <nolonwabomahanjana13@gmail.com>
2017-06-13 15:45:21 +0200

    file changed

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 var template = document.querySelector('.myTemplate').innerHTML;
 var myTemp = Handlebars.compile(template);
+
+var itemList =[{
+  totalBudget : "1000";
+  IconPicture :
+  Price :
+  Catergory :
+
+}]
