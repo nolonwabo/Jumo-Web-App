ceb452387f841cae86bb197c66152bb4380fa19a
sivuyisiwe <sivuyisiwequse0@gmail.com>
2017-06-13 18:05:50 +0200

    index file changes

--- main.js
+++ main.js
@@ -1,71 +1,119 @@
-var template = document.querySelector('.myTemplate').innerHTML;
-var myTemp = Handlebars.compile(template);
 
-var itemList = [{
-      totalBudget: "1000";
-      IconPicture: Price: Catergory:
-    },
-    totalBudget: "1000";
-    IconPicture:
-    Price:
-    Catergory:
-  },
-  totalBudget: "1000";
-IconPicture:
-  Price:
-  Catergory:
-},
-totalBudget: "1000";
-IconPicture:
-  Price:
-  Catergory:
-},
-totalBudget: "1000";
-IconPicture:
-  Price:
-  Catergory:
-},
-totalBudget: "1000";
-IconPicture:
-  Price:
-  Catergory:
-},
-totalBudget: "1000";
-IconPicture:
-  Price:
-  Catergory:
-},
-totalBudget: "1000";
-IconPicture:
-  Price:
-  Catergory:
-},
-totalBudget: "1000";
-IconPicture:
-  Price:
-  Catergory:
-},
-totalBudget: "1000";
-IconPicture:
-  Price:
-  Catergory:
-},
-totalBudget: "1000";
-IconPicture:
-  Price:
-  Catergory:
-},
-totalBudget: "1000";
-IconPicture:
-  Price:
-  Catergory:
-}];
+var container = document.querySelector('.container');
+
+//var template = document.querySelector('.myTemplate').innerHTML;
+//var myTemp = Handlebars.compile(template);
+
+
+function isSelected(target, category){
+  return target.classList.contains(category) && target.classList.contains('selected')
+}
+
+function notSelected(target, category){
+  return target.classList.contains(category) && !target.classList.contains('selected')
+}
+
+var amount = 0;
+
+container.addEventListener('click', function(evt){
+  var target = evt.target;
+
+  if (isSelected(target, 'luxury')){
+      //luxury down
+      amount--;
+  }
+  else if (notSelected(target, 'luxury')) {
+      //luxury up
+      amount++;
+  }
+
+  // if (target.contains('need') && target.contains('selected')){
+  //
+  // }
+
+
+  //if the element have the selected category already we are going to remove it so the counter for that category needs to go down
+
+  //otherwise this is a newly selected category and the counter needs to go up
+
+
+  target.classList.toggle('selected');
+
+
+  //luxury
+  //want
+  //need
+
+})
+
+
+
+
+// var itemList = [{
+//       totalBudget: "1000";
+//       IconPicture: Price: Catergory:
+//     },
+//     totalBudget: "1000";
+//     IconPicture:
+//     Price:
+//     Catergory:
+//   },
+//   totalBudget: "1000";
+// IconPicture:
+//   Price:
+//   Catergory:
+// },
+// totalBudget: "1000";
+// IconPicture:
+//   Price:
+//   Catergory:
+// },
+// totalBudget: "1000";
+// IconPicture:
+//   Price:
+//   Catergory:
+// },
+// totalBudget: "1000";
+// IconPicture:
+//   Price:
+//   Catergory:
+// },
+// totalBudget: "1000";
+// IconPicture:
+//   Price:
+//   Catergory:
+// },
+// totalBudget: "1000";
+// IconPicture:
+//   Price:
+//   Catergory:
+// },
+// totalBudget: "1000";
+// IconPicture:
+//   Price:
+//   Catergory:
+// },
+// totalBudget: "1000";
+// IconPicture:
+//   Price:
+//   Catergory:
+// },
+// totalBudget: "1000";
+// IconPicture:
+//   Price:
+//   Catergory:
+// },
+// totalBudget: "1000";
+// IconPicture:
+//   Price:
+//   Catergory:
+// }];
 
 function submitButton() {
   for (var i = 0; i < itemList.length; i++) {
     var catagoryList = itemList[i];
   }
 if(Catergory === Luxury && Price <= 100){
-  
+
 }
 }
