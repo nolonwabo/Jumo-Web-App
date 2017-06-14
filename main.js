b4b5541f43dabfa7835372350fb1c29bede5c23e
Loyiso <Loyiso@projectcodex.co>
2017-06-14 16:04:14 +0200

    save changes

--- main.js
+++ main.js
@@ -1,139 +1,113 @@
-
-
 var container = document.querySelector('.container');
 
-//var template = document.querySelector('.myTemplate').innerHTML;
-//var myTemp = Handlebars.compile(template);
-
-
-function isSelected(target, category){
-  return target.classList.contains(category) && target.classList.contains('selected')
+function isSelected(target, category) {
+    return target.classList.contains(category) && target.classList.contains('selected')
 }
 
-function notSelected(target, category){
-  return target.classList.contains(category) && !target.classList.contains('selected')
+function notSelected(target, category) {
+    return target.classList.contains(category) && !target.classList.contains('selected')
 }
 
-var amount = 0;
+var categoriesSelectedCount = {
+    'need': 0,
+    'want': 0,
+    'luxury': 0
+};
 
-container.addEventListener('click', function(evt){
-  var target = evt.target;
+var categoryAmounts = {
+    'need': 0,
+    'want': 0,
+    'luxury': 0
+};
 
-  if (isSelected(target, 'luxury')){
-      //luxury down
-      amount--;
-
-  }
-  else if (notSelected(target, 'luxury')) {
-      //luxury up
-      amount++;
-  }
-
-//  if (isSelected (target, 'need')){
-//  amount--;
-//  }
-
-  //else if (notSelected(target, 'need')) {
-      //luxury up
-    //  amount++;
-//  }
-//  if (isSelected(target, 'want')){
-  //  amount--;
-  //}
-  //else if (notSelected(target, 'want'))
-
-
-  //if the element have the selected category already we are going to remove it so the counter for that category needs to go down
-
-  //otherwise this is a newly selected category and the counter needs to go up
-
-
-  target.classList.toggle('selected');
-// var template = document.querySelector('.myTemplate').innerHTML;
-// var myTemp = Handlebars.compile(template);
-var taskId = 0;
-
-var itemList = [{
-      totalBudget: "1000",
-      taskId: 01,
-      Price: "150",
-      Catergory:"need"
-    },
-    totalBudget: "1000",
-    taskId: 02,
-    Price: "50",
-    Catergory: "need",
-  },
-  totalBudget: "1000",
-taskId: 03,
-  Price: "45",
-  Catergory: "need"
+function totalSpend() {
+    console.log(categoryAmounts);
 }
-// totalBudget: "1000",
-// IconPicture: "earings",
-//   Price: "120",
-//   Catergory: "want"
-// },
-// totalBudget: "1000",
-// IconPicture: "handbag",
-//   Price: "300",
-//   Catergory: "want"
-// },
-// totalBudget: "1000",
-// IconPicture: "clothes",
-//   Price: "500",
-//   Catergory: "want"
-// },
-// totalBudget: "1000",
-// IconPicture: "shirt",
-//   Price: "300",
-//   Catergory: "want"
-// },
-// totalBudget: "1000",
-// IconPicture: "hawaiin",
-//   Price: "250",
-//   Catergory: "want"
-// },
-// totalBudget: "1000",
-// IconPicture: "smartphone",
-//   Price: "3500",
-//   Catergory: "luxury"
-// },
-// totalBudget: "1000",
-// IconPicture: "broccoli",
-//   Price: "120",
-//   Catergory: "need"
-// },
-// totalBudget: "1000",
-// IconPicture: "salad",
-//   Price: "150",
-//   Catergory: "want"
-// },
-// totalBudget: "1000",
-// IconPicture: "apple",
-//   Price: "45",
-//   Catergory:"need"
-];
-var purchaseBtn = document.querySelector('#purchaseBtn')
+
+function categoryPercentage() {
+    var principleAmount = 1000;
+    var expectedNeedPercentege = 60 % ;
+    var expectedWantPercentege = 30 % ;
+    var expectedLuxuryPercentege = 10 % ;
+
+    var catagoryAmountsNeedVal = categoryAmounts.need;
+    var catagoryAmountsWantVal = categoryAmounts.want;
+    var catagoryAmountsLuxuryVal = categoryAmounts.luxury;
+
+
+    if (catagoryAmountsNeedVal == true) {
+        var percentageCalc = (catagoryAmountsNeedVal / principleAmount) * 100;
+        if(percentageCalc >= expectedNeedPercentege ){
+
+        }
+    }
+    if (catagoryAmountsWantVal == true) {
+        var percentageCalc = (catagoryAmountsWantVal / principleAmount) * 100;
+        if(percentageCalc <= expectedWantPercentege ){
+
+        }
+    }
+    if (catagoryAmountsLuxuryVal == true) {
+        var percentageCalc = (catagoryAmountsLuxuryVal / principleAmount) * 100;
+        if(percentageCalc <= expectedLuxuryPercentege ){
+
+        }
+    }
+
+}
+
+
+//if the element have the selected category already we are going to remove it so the counter for that category needs to go down
+//otherwise this is a newly selected category and the counter needs to go up
+
+function manageSpendingCategory(target, category) {
+
+    var categoryPrice = Number(target.dataset.categoryPrice);
+
+    if (isSelected(target, category)) {
+        //luxury down
+        categoriesSelectedCount[category]--;
+        categoryAmounts[category] -= categoryPrice;
+
+    } else if (notSelected(target, category)) {
+        //luxury up
+        categoriesSelectedCount[category]++;
+        categoryAmounts[category] += categoryPrice;
+
+}
+
+container.addEventListener('click', function(evt) {
+    var target = evt.target;
+
+    manageSpendingCategory(target, 'need');
+    manageSpendingCategory(target, 'want');
+    manageSpendingCategory(target, 'luxury');
+
+    totalSpend();
+    target.classList.toggle('selected');
+});
+var purchaseBtn = document.querySelector('#purchaseBtn');
 
 purchaseBtn.addEventListener("click", function() {
-  for (var i = 0; i < itemList.length; i++) {
-    var catagoryList = itemList[i];
 
-if(Catergory === luxury && Price <= 100){
+    alert(taskId)
+    for (var i = 0; i < itemList.length; i++) {
+        var catagoryList = itemList[i];
 
-}
+        if (Catergory.taskId === true) {
 
- if (Catergory === luxury && itemList.Price <= 100) {
+        }
+        if (Catergory === luxury && itemList.Price <= 100) {
+            itemList.totalBudget - itemList.Price;
 
-  }
-}
+        }
+    }
+});
 
-})
 
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-}
+// function openNav() {
+//     document.getElementById("myNav").style.height = "100%";
+// }
+// function closeNav() {
+//     document.getElementById("myNav").style.height = "0%";
+// }
